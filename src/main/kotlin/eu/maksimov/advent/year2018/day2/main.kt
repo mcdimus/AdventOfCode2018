8f63e4de0cdc34a04baf42d439636f8754f842a8
Dmitri Maksimov <dmitri.maksimov@nortal.com>
2018-12-03 11:13:39 +0200

    Extracted checksum calculation into extension function.

--- src/main/kotlin/eu/maksimov/advent/year2018/day2/main.kt
+++ src/main/kotlin/eu/maksimov/advent/year2018/day2/main.kt
@@ -3,20 +3,22 @@ package eu.maksimov.advent.year2018.day2
 import eu.maksimov.advent.year2018.shared.readResourceLines
 
 fun main() {
-    var twos = 0
-    var threes = 0
     val boxes = readResourceLines("day2_input").map { Box(it) }
 
     // part 1
-    boxes.forEach {
-        if (it.countsForTwo)
-            twos++
-        if (it.countsForThree)
-            threes++
-    }
-    println("Checksum: ${twos * threes}")
+    println("Checksum: ${boxes.getChecksum()}")
 
     // part 2
     val commonLetters = CorrectBoxesFinder(boxes).getCommonLetters()
     println("Common letters: $commonLetters")
+}
+
+private fun List<Box>.getChecksum(): Int {
+    var twos = 0
+    var threes = 0
+    this.forEach {
+        if (it.countsForTwo) twos++
+        if (it.countsForThree) threes++
+    }
+    return twos * threes
 }
